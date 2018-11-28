7abb193108874f0669c5d8bdeda0cd544229da1f
Diogo Magalhaes <darkflashyts@gmail.com>
2018-11-28 11:17:04 +0000

    SideMenu dedicated files
    CSS e JS dedicados ao SideMenu

--- JS/Reservas.js
+++ JS/Reservas.js
@@ -1,12 +1,4 @@
-﻿function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-// Get the modal
+﻿// Get the modal
 var modal = document.getElementById('id01');
 
 var modal = document.getElementById('id02');
