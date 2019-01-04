eca2790949fe24a55fb32d8fc4c5597d9f7cf273
Flash <darkflashyts@gmail.com>
2019-01-04 21:39:09 +0000

    Gerir grupos
    Vai buscar o nome dos grupos a que o utilizador pretence

--- Gerir_Grupos.php
+++ Gerir_Grupos.php
@@ -1,15 +1,34 @@
 ﻿<?php
 session_start();
-?>
-<?php
 
-print_r($_SESSION);
+function groupName(){
+    $db = mysqli_connect('localhost', 'root', '', 'lrm_v2');
+
+    $username = $_SESSION['myusername'];
+
+    $useridquery = "SELECT id_utilizador FROM utilizador WHERE uti_username = '$username'";
+    $resuserid = mysqli_query($db, $useridquery);
+    $rowuserid = mysqli_fetch_array($resuserid);
+    $userid = $rowuserid['id_utilizador'];
+
+    $groupidquery = "SELECT gru_uti_id_grupo FROM grupo_utilizador WHERE gru_uti_id_utilizador = '$userid'";
+    $resgroupid = mysqli_query($db, $groupidquery);
+    $rowgroupid = mysqli_fetch_array($resgroupid);
+    $groupid = $rowgroupid['gru_uti_id_grupo'];
+
+    $groupnamequery = "SELECT grup_nome FROM grupo WHERE id_grupo = '$groupid'";
+    $resgroupname = mysqli_query($db, $groupnamequery);
+    $rowgroupname = mysqli_fetch_array($resgroupname);
+    $groupname = $rowgroupname['grup_nome'];
+
+    echo $groupname;
+}
 ?>
 <html>
 <link rel="stylesheet" type="text/css" href="../TalkALot/CSS/Index.css">
 <link rel="stylesheet" type="text/css" href="../TalkALot/CSS/Animate.css">
    <head>
-      <title>Login Page</title>
+      <title>Gerir Grupos</title>
 
       <style type = "text/css">
          body {
@@ -70,14 +89,7 @@ ul {
             
             <div style = "margin:30px">
 
-               <form action = "../TALKALOT/PHP/login.php" method = "post">
-                  <label  class = "animated fadeInUp">UserName  :</label><input type = "text" name = "uname" class = "box animated fadeInUp"/><br /><br />
-                  <label class = "animated fadeInUp">Password  :</label><input type = "password" name = "psw" class = "box animated fadeInUp" /><br/><br /><br />
-                  <input type="Button" value = " Voltar" class="actionbutton animated fadeInLeft" style=" background-color: red;float:left;"/>
-                  <input type = "submit" value = " Iniciar Sessao" class="actionbutton animated fadeInRight" style=" background-color: #4CAF50;float:right;"/><br /><br><br>
-               </form>
-
-
+<?php groupName(); ?> 
 
             </div>
 
