5f97c1a8839b622e1885bf01d9319fef9bbff7b8
Flash <darkflashyts@gmail.com>
2019-01-11 01:38:56 +0000

    Text index's

--- Index.php
+++ Index.php
@@ -83,13 +83,7 @@ ul {
       </div>
       <div style="height: 300px">
          <p style="font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;text-align:center;font-size:40px;margin-top: 2%;padding-bottom: 5px;font-weight: bold">O nosso Objetivo</p>
-         <p style="font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;text-align:justify;margin-left: 40px; margin-right: 40px">Pellentesque gravida justo sit amet dapibus tempus. Maecenas egestas aliquam velit, sed hendrerit felis malesuada nec.
-            Donec ac justo eget est fermentum euismod sit amet ac odio. Praesent luctus enim enim, a fermentum nisl luctus 
-            a. Nunc vulputate et dui eu pulvinar. Cras metus felis, lobortis sed tincidunt malesuada, egestas in eros. Nulla ultrices
-            venenatis magna sit amet ornare. Pellentesque dictum rhoncus ullamcorper. Proin hendrerit dapibus elementum.
-            Nam sodales dolor id mi imperdiet ultricies. Ut eu nisi ut odio tincidunt tempus quis sed lectus. Sed elit eros, congue at 
-            leo sit amet, aliquet porta mi. Integer rutrum sed nunc sit amet commodo. Donec sodales dolor at mi congue, 
-            nec pulvinar purus ullamcorper. In lacus turpis, egestas vel nibh at, elementum dignissim erat.
+         <p style="font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;text-align:justify;margin-left: 40px; margin-right: 40px">O nosso objetivo com a realização deste programa é a gestão das respetivas salas de trabalho que fazem parte da mediateca, esse controlo será feira através de uma plataforma web.</br>O nosso programa têm diversas funcionalidades sendo elas o login, a reserva das salas de trabalho, criação de grupos de trabalho, entre outras funcionalidades. </br>Com este sistema acreditamos que será uma mais valia para a nossa escola e que irá aumentará o número utilização das salas trabalho, como  também irá melhorar a organização das mesmas, pois a nossa aplicação oferece layouts compreensíveis e acessíveis para a comunidade escolar.
          </p>
          <br>
          <br>
@@ -105,16 +99,10 @@ ul {
                <td class="circle"></td>
             </tr>
             <tr>
-               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-weight: bold;font-size: 20px;color: white;line-height: 1.2">Donec</td>
-               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-weight: bold;font-size: 20px;color: white;line-height: 1.2">Fermentum</td>
-               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-weight: bold;font-size: 20px;color: white;line-height: 1.2";>Luctus</td>
-               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-weight: bold;font-size: 20px;color: white;line-height: 1.2">Justo</td>
-            </tr>
-            <tr>
-               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-size: 15px;color: white;line-height: 1.2">Donec lorem ipsum</td>
-               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-size: 15px;color: white;line-height: 1.2">Fermentum lorem ipsum</td>
-               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-size: 15px;color: white;line-height: 1.2";>Luctus lorem ipsum</td>
-               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-size: 15px;color: white;line-height: 1.2">Justo lorem ipsum</td>
+               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-weight: bold;font-size: 20px;color: white;line-height: 1.2">Organização</td>
+               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-weight: bold;font-size: 20px;color: white;line-height: 1.2">Facilidade de Reservas</td>
+               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-weight: bold;font-size: 20px;color: white;line-height: 1.2";>Aumento de uso das salas</td>
+               <td style="text-align: center;font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-weight: bold;font-size: 20px;color: white;line-height: 1.2">Gestão do uso das salas</td>
             </tr>
          </table>
       </div>
